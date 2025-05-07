e30acc01fa8436c6a459c3900a991d7929b92e82
Alec Musasa <alecmus@live.com>
2025-05-07 21:50:51 +0200

    Add DeveloperService and stub for create impl

---
+++ src/main/java/com/github/alecmus/tddspring/service/DeveloperService.java
@@ -0,0 +1,10 @@
+package com.github.alecmus.tddspring.service;
+
+import com.github.alecmus.tddspring.dto.DeveloperDTO;
+
+/**
+ * @author Alec Musasa
+ * @since 07 May 2025
+ */
+public interface DeveloperService extends CrudService<DeveloperDTO> {
+}
---
+++ src/main/java/com/github/alecmus/tddspring/service/impl/DeveloperServiceImpl.java
@@ -0,0 +1,18 @@
+package com.github.alecmus.tddspring.service.impl;
+
+import com.github.alecmus.tddspring.dto.DeveloperDTO;
+import com.github.alecmus.tddspring.service.DeveloperService;
+import org.springframework.stereotype.Service;
+
+/**
+ * @author Alec Musasa
+ * @since 07 May 2025
+ */
+@Service
+public class DeveloperServiceImpl implements DeveloperService {
+
+    @Override
+    public DeveloperDTO create(DeveloperDTO dto) {
+        return null;
+    }
+}
